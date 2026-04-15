d5440f90478a328024a23da4b8de6e562ed8f48e
Yeshmant Kumar <yeshmant@gmail.com>
2026-04-15 17:32:29 +0530

    This is the extension project for net banking with some extra added features to it. And this is first commit.

---
+++ src/main/java/com/Scalable_payment_system/ScalablePaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Scalable_payment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalablePaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalablePaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Scalable_payment_system/ScalablePaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Scalable_payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalablePaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
